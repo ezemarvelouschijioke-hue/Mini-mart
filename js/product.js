c186ac94c40b076b14c8de3d7be94e934774121a
ezemarvelouschijioke-hue <ezemarvelouschijioke@gmail.com>
2026-01-06 22:51:57 +0100

    Create js/products.js

---
+++ js/product.js
@@ -0,0 +1,20 @@
+const products = [
+    {
+        id: 1,
+        name: "Wireless Headphones",
+        price: 25000,
+        image: "assets/images/headphones.jpg"
+    },
+    {
+        id: 2,
+        name: "Smart Watch",
+        price: 45000,
+        image: "assets/images/watch.jpg"
+    },
+    {
+        id: 3,
+        name: "Leather Wallet",
+        price: 10000,
+        image: "assets/images/wallet.jpg"
+    }
+];
